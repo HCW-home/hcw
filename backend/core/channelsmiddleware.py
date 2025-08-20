6f2e7cdf379fff130483604782d0aa6ba520ee8d
Olivier Bitsch <olivier.b@iabsis.com>
2025-08-20 14:51:09 +0200

    Fix websocket auth

--- backend/core/channelsmiddleware.py
+++ backend/core/channelsmiddleware.py
@@ -8,25 +8,14 @@ from jwt import decode as InvalidTokenError
 
 @database_sync_to_async
 def get_user(validated_token):
-    try:
-        user = JWTAuthentication().get_user(validated_token)
-        return user
-    except:
-        return None
-
+    return JWTAuthentication().get_user(validated_token)
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_string = parse_qs(scope["query_string"].decode())
         token = query_string.get("token", [None])[0]
-        if token is None:
-            scope["user"] = None
-            return await super().__call__(scope, receive, send)
 
-        try:
-            validated_token = JWTAuthentication().get_validated_token(token)
-            scope["user"] = await get_user(validated_token)
-        except InvalidTokenError:
-            scope["user"] = None
+        validated_token = JWTAuthentication().get_validated_token(token)
+        scope["user"] = await get_user(validated_token)
 
         return await super().__call__(scope, receive, send)
