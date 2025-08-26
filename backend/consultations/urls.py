09852658db3ca580cf2c835b38d4e1b97964c418
Olivier Bitsch <olivier.b@iabsis.com>
2025-08-26 10:46:25 +0200

    Cleanup

--- backend/consultations/urls.py
+++ backend/consultations/urls.py
@@ -2,19 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     ConsultationViewSet, 
-    GroupViewSet, 
-    AppointmentViewSet,
-    ParticipantViewSet,
-    MessageViewSet
+    GroupViewSet
 )
 
 # DRF router configuration
 router = DefaultRouter()
 router.register(r'consultations', ConsultationViewSet, basename='consultation')
 router.register(r'groups', GroupViewSet, basename='group')
-# router.register(r'appointments', AppointmentViewSet, basename='appointment')
-# router.register(r'participants', ParticipantViewSet, basename='participant')
-# router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('api/', include(router.urls)),
