0b52a9854c631e4ab07f7fd20c7e4087daa86325
Olivier Bitsch <olivier.b@iabsis.com>
2025-09-15 13:48:12 +0200

    Update the way to get user timezone

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -1,4 +1,4 @@
-import pytz
+from zoneinfo import ZoneInfo
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
@@ -9,25 +9,27 @@ class TimezoneMiddleware:
     """
     Middleware to activate user's timezone for the duration of the request.
     """
-    
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         if request.user.is_authenticated:
             try:
-                user_timezone = pytz.timezone(request.user.timezone)
-                timezone.activate(user_timezone)
-            except (AttributeError, pytz.exceptions.UnknownTimeZoneError):
-                # If user has no timezone or invalid timezone, use default
+                if request.user.timezone:
+                    user_timezone = ZoneInfo(request.user.timezone)
+                    timezone.activate(user_timezone)
+                else:
+                    timezone.deactivate()
+            except Exception:
+                # If user has no valid timezone, use default
                 timezone.deactivate()
         else:
-            # If user is not authenticated, use default timezone
             timezone.deactivate()
 
         response = self.get_response(request)
-        
+
         # Clean up after request
         timezone.deactivate()
-        
+
         return response
