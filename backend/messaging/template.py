bf9c4327079112f867fa0ff0e69b908a7aa67a44
Olivier Bitsch <olivier.b@iabsis.com>
2025-12-29 09:49:21 +0100

    Change appointment info

--- backend/messaging/template.py
+++ backend/messaging/template.py
@@ -26,9 +26,11 @@ DEFAULT_NOTIFICATION_MESSAGES = {
         "helper_text": "Message sent to user when a beneficiary sends a message",
     },
     "invitation_to_appointment": {
-        "subject": _("Your appointment"),
+        "subject": _("Your consultation has been scheduled"),
         "content": _(
-            "You receive this message because you have been invited to join a consultation at a later time. Please log in to the system to view the details and join the consultation when you are ready."
+            """Dear {{ obj.name }},\n"""
+            """Your consultation has been successfully scheduled."""
+            """Appointment Details: {{ obj.appointment.scheduled_at}}"""
         ),
         "model": "consultations.Participant",
         "helper_text": "Message sent to user with invitation to join a consultation at a later time",
